6dc13894d83f39f8666557bc13dbd46c4570fa5b
Ángel Gaudes <a25106@svalero.com>
2022-11-23 18:44:36 +0100

    ServerLine Entity Created

---
+++ Entities/ServerLine.cs
@@ -0,0 +1,10 @@
+class ServerLine
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ServerId { get; set; }
+    public string? Nickname { get; set; }
+    public DateTime? JoinDate { get; set; }
+    public bool isActive { get; set; }
+
+}

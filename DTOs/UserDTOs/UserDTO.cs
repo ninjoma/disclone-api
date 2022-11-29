c2159a04484851f831080879a7196f26b0e3f5df
DavidTorrubia <a25585@svalero.com>
2022-11-29 20:28:27 +0100

    Creacion de UsersDTO

--- DTOs/UserDTOs/UserDTO.cs
+++ DTOs/UserDTOs/UserDTO.cs
@@ -1,6 +1,14 @@
 ﻿namespace disclone_api.DTOs.UserDTOs
 {
-    public class Class
+    public class UserDTO
     {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public  string? Image { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }

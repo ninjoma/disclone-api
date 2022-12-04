61e8fa5fa87cfb682a31ce31aa6b430bb838f147
unknown <davidtorrubiasantos@gmail.com>
2022-12-04 15:05:32 +0100

    Creacion de los DTOs de member

---
+++ DTOs/MemberDTOs/MemberDTO.cs
@@ -0,0 +1,17 @@
+﻿using disclone_api.DTOs.ServerDTOs;
+using disclone_api.DTOs.UserDTOs;
+
+namespace disclone_api.DTOs.MemberDTOs
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ServerId { get; set; }
+        public string? NickName { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool IsActive { get; set; }
+        public virtual UserDTO? User { get; set; }
+        public virtual ServerDTO? Server { get; set; }
+    }
+}

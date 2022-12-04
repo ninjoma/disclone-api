a210d593c8a66ca8c02a5f2b751cc2681528b98b
unknown <davidtorrubiasantos@gmail.com>
2022-12-04 18:49:36 +0100

    Implementado el controlador de Invitations

--- Controllers/InvitationsController.cs
+++ Controllers/InvitationsController.cs
@@ -1,38 +1,109 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using disclone_api.Entities;
+using disclone_api.Services.InvitationServices;
+using disclone_api.DTOs.InvitationDTOs;
 
 namespace disclone_api.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class InvitationsController : ControllerBase
 {
+    #region Constructor
     private readonly DataContext _context;
     private readonly ILogger<InvitationsController> _logger;
-    public InvitationsController(DataContext context, ILogger<InvitationsController> logger)
+    private readonly IInvitationService _InvitationSv;
+    public InvitationsController(DataContext context, ILogger<InvitationsController> logger, IInvitationService InvitationSv)
     {
         _context = context;
         _logger = logger;
+        _InvitationSv = InvitationSv;
     }
+    #endregion
 
-    [HttpGet("{id}")]
-    public Invitation? GetById(int id)
-    {  
-        return _context.Invitation.Find(id);
-    }
-
-    [HttpPost("create/")]
-    public void Create(int serverId, int Receiver, string url)
+    #region Get
+    [HttpGet("GetById/{id}")]
+    public async Task<ActionResult> GetById(int id)
     {
-        Invitation invitation = new Invitation() {
-            ServerId = serverId,
-            Receiver = Receiver,
-            Url = url,
-            CreationDate = DateTime.Now.ToUniversalTime(),
-            IsActive = true,
-        };
-
-        _context.Invitation.Add(invitation);
-        _context.SaveChanges();
+        var result = await _InvitationSv.GetById(id);
+        if (result != null) 
+        {
+            return Ok(result);
+        } else
+        {
+            return BadRequest();
+        }
     }
 
+    [HttpGet("GetByServerIdAndByUserId/{userId}/{serverId}")]
+    public async Task<ActionResult> GetByServerIdAndByUserId(int userId, int serverId)
+    {
+        var result = await _InvitationSv.GetByServerIdAndByUserId(userId, serverId);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("ListByserverId/{id}")]
+    public async Task<ActionResult> ListByserverId(int id)
+    {
+        var result = await _InvitationSv.ListByServerId(id);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("ListByUserId/{id}")]
+    public async Task<ActionResult> ListByUserId(int id)
+    {
+        var result = await _InvitationSv.ListByUserId(id);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+    #endregion
+
+    #region Set
+    [HttpPost("AddEditAsync")]
+    public async Task<ActionResult> AddEditAsync(InvitationDTO invitation)
+    {
+        var result = await _InvitationSv.AddEditAsync(invitation);
+        if (result != null)
+        {
+            return Ok(result);
+        } else
+        {
+            return BadRequest();
+        }
+    }
+    #endregion
+
+    #region Delete
+    [HttpDelete("ToggleInactiveById/{id}")]
+    public async Task<ActionResult> ToggleInactiveById(int id)
+    {
+        var result = await _InvitationSv.ToggleInactiveById(id);
+        if (result != null)
+        {
+            return Ok(result);
+        } else
+        {
+            return BadRequest();
+        }
+    }
+    #endregion
 }

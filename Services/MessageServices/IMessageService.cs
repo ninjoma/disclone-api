47ce455073b18c30586f618126b2a11b7165eb22
DavidTorrubia <a25585@svalero.com>
2022-12-07 18:52:22 +0100

    Creacion de los servicios e interfaces

---
+++ Services/MessageServices/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using disclone_api.DTOs.MessageDTOs;
+
+namespace disclone_api.Services.MessageServices
+{
+    public interface IMessageService
+    {
+        Task<MessageDTO> GetById(int id, bool isActive);
+        Task<List<MessageDTO>> ListByChannelId(int channelId, bool isActive);
+        Task<List<MessageDTO>> ListByUserId(int userId, bool isActive);
+        Task<MessageDTO> AddEditAsync(MessageDTO message);
+        Task<MessageDTO> ToggleInactiveById(int id);
+    }
+}
---
+++ Services/MessageServices/MessageService.cs
@@ -0,0 +1,40 @@
+﻿using disclone_api.DTOs.MessageDTOs;
+
+namespace disclone_api.Services.MessageServices
+{
+    public class MessageService : IMessageService
+    {
+        public Task<MessageDTO> AddEditAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<MessageDTO> CreateMessageAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<MessageDTO> UpdateMessageAsync(MessageDTO message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MessageDTO> GetById(int id, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MessageDTO>> ListByChannelId(int channelId, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MessageDTO>> ListByUserId(int userId, bool isActive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MessageDTO> ToggleInactiveById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

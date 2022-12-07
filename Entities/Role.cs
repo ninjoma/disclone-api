402b3eb102f590fd96738abe84e55c9b7a576471
DavidTorrubia <a25585@svalero.com>
2022-12-07 17:57:15 +0100

    Traspaso del enum de roles a un archivo aparte

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -1,29 +1,7 @@
 using disclone_api.Entities;
+using disclone_api.Enums;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
-// TODO: Implementar el enum en un archivo aparte
-[Flags]
-public enum Permissions
-{
-    None = 0,
-    CanEraseMessages = 1,
-    CanWrite = 2,
-    EnterChannel = 4,
-    LeaveChannel = 8,
-    CanMute = 16,
-    CanMuteOthers = 32,
-    CanKick = 64,
-    CanBan = 128,
-    CanDeafOthers = 256,
-    CanModifyRole = 512,
-    CanModifyChannel = 1024,
-    CanModifyServer = 2048,
-    CanChangeNickname = 4096,
-    CanChangeOtherNicknames = 8192,
-    CanMoveOthers = 16384,
-    CanCreateInvitation = 32768,
-}
 
 public class Role
 {
@@ -31,7 +9,7 @@ public class Role
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Color { get; set; }
-    public Permissions Permits { get; set; }
+    public RolePermissions Permits { get; set; }
     public DateTime CreationDate {get; set; }
     public bool isActive {get; set; }
     public virtual Server? Server { get; set; }
---
+++ Enums/RolePermissionsEnum.cs
@@ -0,0 +1,24 @@
+﻿namespace disclone_api.Enums
+{
+    [Flags]
+    public enum RolePermissionsEnum
+    {
+        None = 0,
+        CanEraseMessages = 1,
+        CanWrite = 2,
+        EnterChannel = 4,
+        LeaveChannel = 8,
+        CanMute = 16,
+        CanMuteOthers = 32,
+        CanKick = 64,
+        CanBan = 128,
+        CanDeafOthers = 256,
+        CanModifyRole = 512,
+        CanModifyChannel = 1024,
+        CanModifyServer = 2048,
+        CanChangeNickname = 4096,
+        CanChangeOtherNicknames = 8192,
+        CanMoveOthers = 16384,
+        CanCreateInvitation = 32768,
+    }
+}

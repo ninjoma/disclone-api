7912505c7c64d830892ae29e97e8f349ba1fb457
unknown <davidtorrubiasantos@gmail.com>
2022-12-08 14:55:27 +0100

    Implementacion de los nuevos DTOs

--- DTOs/MappingProfile.cs
+++ DTOs/MappingProfile.cs
@@ -16,25 +16,40 @@ namespace disclone_api.DTOs
             CreateMap<User, UserDTO>()
                 .ForMember(dest => dest.Id, act => act.MapFrom(src => src.Id))
                 .ReverseMap();
+
+            CreateMap<User, UserGridDTO>()
+                .ReverseMap();
             #endregion
 
             #region Server
             CreateMap<Server, ServerDTO>()
+                .ReverseMap();
+
+            CreateMap<Server, ServerGridDTO>()
                 .ReverseMap();
             #endregion
 
             #region Member
             CreateMap<Member, MemberDTO>()
                 .ReverseMap();
+
+            CreateMap<Member, MemberGridDTO>()
+                .ReverseMap();
             #endregion
 
             #region Invitation
             CreateMap<Invitation, InvitationDTO>()
                 .ReverseMap();
+
+            CreateMap<Invitation, MemberGridDTO>()
+                .ReverseMap();
             #endregion
 
             #region Channel
             CreateMap<Channel, ChannelDTO>()
+                .ReverseMap();
+
+            CreateMap<Channel, ChannelGridDTO>()
                 .ReverseMap();
             #endregion
         }
--- DTOs/UserDTOs/UserGridDTO.cs
+++ DTOs/UserDTOs/UserGridDTO.cs
@@ -5,15 +5,18 @@ using disclone_api.DTOs.ServerDTOs;
 
 namespace disclone_api.DTOs.UserDTOs
 {
-    public class UserDTO
+    public class UserGridDTO
     {
-
         public int Id { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
-        public string? Password {get; set; }
-        public  string? Image { get; set; }
+        public string? Password { get; set; }
+        public string? Image { get; set; }
         public DateTime CreationDate { get; set; }
         public bool IsActive { get; set; }
+        public virtual List<MemberDTO>? Members { get; set; }
+        public virtual List<InvitationDTO>? Invitations { get; set; }
+        public virtual List<MessageDTO>? Messages { get; set; }
+        public virtual List<ServerDTO>? Servers { get; set; }
     }
 }

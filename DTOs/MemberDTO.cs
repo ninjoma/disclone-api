f5bd19bb2fc24cc2ac61d317b307987125d71d0f
Ninjam <juanjosegutierrez107@gmail.com>
2023-01-31 15:36:49 +0100

    Fixed Typo

--- DTOs/MemberDTO.cs
+++ DTOs/MemberDTO.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int ServerId { get; set; }
-        public string? NickName { get; set; }
+        public string? Nickname { get; set; }
         public DateTime JoinDate { get; set; }
         public bool IsActive { get; set; }
         public virtual UserDTO? User { get; set; }
